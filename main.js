92ba8b515f8baa27a3d55f90f1f461feec5787ab
otavio <otavioaam@gmail.com>
2024-05-21 20:01:53 -0300

    commit incial

---
+++ main.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+    $("form").submit(function (event) {
+        event.preventDefault();
+
+        const valorinput = $("#tarefa").val();
+        $("ul").append(`<li><span class="pendente">📍</span> ${valorinput}</li>`);
+        $("#tarefa").val('');
+
+        $("ul").on('click', 'li', function(){
+            $(this).toggleClass("linha-meio");
+            $(this).toggleClass("checked");
+            $(this).find(".pendente").toggleClass("hide");
+            
+        })
+
+        
+    });
+
+});
